6aaf7713746466566ea08378d3e4ded4d05cbe1e
NazarLukashik <nazarlykashik@gmail.com>
2022-04-14 12:33:39 +0200

    Initial Commit

---
+++ Konstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Konstraints
+//
+//  Created by Nazar Lykashik on 14.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
